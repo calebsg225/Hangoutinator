32d17587b84c975bc2bf8b8277efeb7ba3b5d369
calebsg225 <calebsg2253284@gmail.com>
2025-09-23 01:09:47 -0400

    organize 'welcome role' feature in a features folder

---
+++ src/features/welcome_role.rs
@@ -0,0 +1,96 @@
+//! src/features/welcome_role.rs
+
+use std::collections::{HashMap, HashSet};
+
+use serenity::{
+    Client,
+    all::{
+        ChannelId, Context, CreateMessage, GuildId, GuildMemberUpdateEvent, Mentionable, RoleId,
+        UserId,
+    },
+    prelude::TypeMapKey,
+};
+
+// collection to keep track of members without the verified role
+pub struct UnverifiedMemberCollection;
+
+impl TypeMapKey for UnverifiedMemberCollection {
+    type Value = HashMap<GuildId, HashSet<UserId>>;
+}
+
+/// Checks if `UnverifiedMemberCollection` contains a specific member in a specific guild
+async fn is_unverified_member(ctx: &Context, guild_id: GuildId, user_id: UserId) -> bool {
+    let data = ctx.data.write().await;
+    // TODO: deal with these fucking unwrap()s!!
+    let unverified_members = data
+        .get::<UnverifiedMemberCollection>()
+        .unwrap()
+        .get(&guild_id)
+        .unwrap();
+    return unverified_members.contains(&user_id);
+}
+
+/// Removes a verified member from `UnverifiedMembersCollection`
+async fn remove_verified_member(ctx: &Context, guild_id: GuildId, user_id: UserId) -> bool {
+    let mut data = ctx.data.write().await;
+    let unverified_members = data
+        .get_mut::<UnverifiedMemberCollection>()
+        .unwrap()
+        .get_mut(&guild_id)
+        .unwrap();
+    unverified_members.remove(&user_id)
+}
+
+/// checks if a member has been verified. If so, sends a welcome message.
+pub async fn welcome_verified_member(
+    ctx: &Context,
+    event: &GuildMemberUpdateEvent,
+    verified_role_id: &RoleId,
+    welcome_channel_id: &ChannelId,
+) {
+    // check that the member is in `UnverifiedMemberCollection`.
+    let is_unverified = is_unverified_member(&ctx, event.guild_id, event.user.id).await;
+    // check that the member has the role.
+    let member_has_role = event.roles.contains(&verified_role_id);
+
+    if is_unverified && member_has_role {
+        remove_verified_member(&ctx, event.guild_id, event.user.id).await;
+        let welcome_message = format!("Welcome, {}!", event.user.mention());
+        let _ = welcome_channel_id
+            .send_message(&ctx.http, CreateMessage::new().content(welcome_message))
+            .await;
+    }
+}
+
+/// populates the `UnverifiedMemberCollection` collection on `client.data` with unverified members
+/// from all active guilds
+pub async fn populate_unverified_members(client: &Client, verified_role_id: &RoleId) {
+    // fetch guilds the bot is in
+    // NOTE: Further steps required to retrieve more than 100 guilds.
+    let active_guilds = client.http.get_guilds(None, Some(100)).await.unwrap();
+
+    {
+        // in this scope: populate `UnverifiedMemberCollection` with unverified members from all
+        // guilds
+        let mut data = client.data.write().await;
+        let global_unverified_members = data.get_mut::<UnverifiedMemberCollection>().unwrap();
+        println!("Active guild count: {}", active_guilds.len());
+        for guild in active_guilds.iter() {
+            // fetch guild members for each guild the bot is in
+            // NOTE: Further steps required to retrieve more than 1000 members.
+            let guild_members = guild.id.members(&client.http, None, None).await.unwrap();
+            let unverified_guild_members: HashSet<UserId> = HashSet::from_iter(
+                guild_members
+                    .iter()
+                    .filter(|m| !m.roles.contains(&verified_role_id))
+                    .map(|m| m.user.id),
+            );
+            println!(
+                "Unverified member count in guild {}: {:?}",
+                guild.id,
+                unverified_guild_members.len()
+            );
+            global_unverified_members.insert(guild.id, unverified_guild_members);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,19 @@
-//! main.rs
+//! src/main.rs
 
-// TODO: improve file structure. Right now it's all in `main.rs`.
-
-use std::{
-    collections::{HashMap, HashSet},
-    env,
-};
+use std::{collections::HashMap, env};
 
 use serenity::{
     Client,
     all::{
-        ChannelId, Context, CreateMessage, EventHandler, GatewayIntents, GuildId,
-        GuildMemberUpdateEvent, Member, Mentionable, OnlineStatus, Ready, RoleId, UserId,
+        ChannelId, Context, EventHandler, GatewayIntents, GuildMemberUpdateEvent, Member,
+        OnlineStatus, Ready, RoleId,
     },
     async_trait,
-    prelude::TypeMapKey,
 };
 
-// collection to keep track of members without the verified role
-struct UnverifiedMemberCollection;
+mod features;
 
-impl TypeMapKey for UnverifiedMemberCollection {
-    type Value = HashMap<GuildId, HashSet<UserId>>;
-}
-
-/// Checks `UnverifiedMemberCollection` contains a specific user in a specific guild
-async fn is_unverified_member(ctx: &Context, guild_id: GuildId, user_id: UserId) -> bool {
-    let data = ctx.data.write().await;
-    // TODO: deal with these fucking unwrap()s!!
-    let unverified_members = data
-        .get::<UnverifiedMemberCollection>()
-        .unwrap()
-        .get(&guild_id)
-        .unwrap();
-    return unverified_members.contains(&user_id);
-}
-
-/// Removes a verified user from `UnverifiedMembersCollection`
-async fn remove_verified_member(ctx: &Context, guild_id: GuildId, user_id: UserId) -> bool {
-    let mut data = ctx.data.write().await;
-    let unverified_members = data
-        .get_mut::<UnverifiedMemberCollection>()
-        .unwrap()
-        .get_mut(&guild_id)
-        .unwrap();
-    unverified_members.remove(&user_id)
-}
+use features::welcome_role;
 
 struct Handler;
 
@@ -58,10 +26,7 @@ impl EventHandler for Handler {
         _: Option<Member>,
         event: GuildMemberUpdateEvent,
     ) {
-        // get the role id and channel id from .env
         // NOTE: Storing the channel id and role id in .env will only work for a single guild.
-        // TODO: multi-guild support (with a database)
-        // TODO: deal with these fucking unwrap()s!!
         let verified_role_id: RoleId = RoleId::from(
             env::var("VERIFIED_ROLE_ID")
                 .expect("Expected a verified role id in the environment.")
@@ -77,18 +42,8 @@ impl EventHandler for Handler {
 
         println!("A guild member update has occured.");
 
-        // check that the user is in `UnverifiedMemberCollection`.
-        let is_unverified = is_unverified_member(&ctx, event.guild_id, event.user.id).await;
-        // check that the user has the role.
-        let user_has_role = event.roles.contains(&verified_role_id);
-
-        if is_unverified && user_has_role {
-            remove_verified_member(&ctx, event.guild_id, event.user.id).await;
-            let welcome_message = format!("Welcome, {}!", event.user.mention());
-            let _ = welcome_channel_id
-                .send_message(&ctx.http, CreateMessage::new().content(welcome_message))
-                .await;
-        }
+        welcome_role::welcome_verified_member(&ctx, &event, &verified_role_id, &welcome_channel_id)
+            .await;
     }
 
     /// runs when the bot is ready
@@ -106,7 +61,6 @@ async fn main() {
     let token = env::var("TOKEN").expect("Expected a token in the environment.");
 
     // NOTE: Not compatable with more than a single guild.
-    // TODO: Use a database.
     let verified_role_id: RoleId = RoleId::from(
         env::var("VERIFIED_ROLE_ID")
             .expect("Expected a verified role id in the environment.")
@@ -129,42 +83,11 @@ async fn main() {
     {
         // in this scope: add collections to client
         let mut data = client.data.write().await;
-        data.insert::<UnverifiedMemberCollection>(HashMap::default());
+        data.insert::<welcome_role::UnverifiedMemberCollection>(HashMap::default());
     }
 
-    // fetch guilds the bot is in
-    // NOTE: Further steps required to retrieve more than 100 guilds.
-    let active_guilds = client.http.get_guilds(None, Some(100)).await.unwrap();
+    welcome_role::populate_unverified_members(&client, &verified_role_id).await;
 
-    {
-        // in this scope: populate `UnverifiedMemberCollection` with unverified members from all
-        // guilds
-        let mut data = client.data.write().await;
-        let global_unverified_members = data.get_mut::<UnverifiedMemberCollection>().unwrap();
-        println!("Active guild count: {}", active_guilds.len());
-        for guild in active_guilds.iter() {
-            // fetch guild members for each guild the bot is in
-            // NOTE: Further steps required to retrieve more than 1000 members.
-            let guild_members = guild.id.members(&client.http, None, None).await.unwrap();
-            let unverified_guild_members: HashSet<UserId> = HashSet::from_iter(
-                guild_members
-                    .iter()
-                    .filter(|m| !m.roles.contains(&verified_role_id))
-                    .map(|m| m.user.id),
-            );
-            println!(
-                "Unverified member count in guild {}: {:?}",
-                guild.id,
-                unverified_guild_members.len()
-            );
-            global_unverified_members.insert(guild.id, unverified_guild_members);
-        }
-    }
-
-    // start a single shard, and start listening to events
-    //
-    // shards will automatically attempt to reconnect, and will preform exponential backoff until
-    // it reconnects
     if let Err(why) = client.start().await {
         println!("Client error: {why:?}");
     }
