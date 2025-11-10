bf37276c0ec08ae4f0282a08e2d1bd0506bb8e50
calebsg225 <calebsg2253284@gmail.com>
2025-11-10 18:18:38 -0500

    Update initial migration
    - change primary keys in linker tables to a combination of the foreign
      keys

--- migrations/20251029191145_initial_migration.sql
+++ migrations/20251029191145_initial_migration.sql
@@ -9,14 +9,15 @@ CREATE TABLE todo (
 -- individual meetup group events
 CREATE TABLE meetup_events (
 	meetup_event_id NUMERIC(12) NOT NULL UNIQUE,
-	PRIMARY KEY (meetup_event_id),
 	-- the meetup groups the event belongs to
 	meetup_group_id NUMERIC(12) NOT NULL,
 	event_hash TEXT NOT NULL,
 	-- keep track of duplicate events across meetup groups
 	duplicate_event_hash TEXT NOT NULL,
 	-- keep track of end time to be removed from db
-	end_time timestamptz NOT NULL
+	end_time timestamptz NOT NULL,
+
+	PRIMARY KEY (meetup_event_id)
 );
 
 -- all discord events from all guilds bot is active in
@@ -28,31 +29,33 @@ CREATE TABLE discord_events (
 -- meetup groups to keep track of
 CREATE TABLE meetup_groups (
 	group_id NUMERIC(12) NOT NULL UNIQUE,
-	PRIMARY KEY (group_id),
-	group_name TEXT NOT NULL UNIQUE
+	group_name TEXT NOT NULL UNIQUE,
+	PRIMARY KEY (group_id)
 );
 
 -- discord guild information
 CREATE TABLE guilds (
 	guild_id NUMERIC(20) NOT NULL UNIQUE,
-	PRIMARY KEY (guild_id),
 	welcome_role_id NUMERIC(22),
 	welcome_channel_id NUMERIC(20),
-	admin_role_id NUMERIC(22)
+	admin_role_id NUMERIC(22),
+	PRIMARY KEY (guild_id)
 );
 
 -- Create 'linker' table between `meetup_groups` and `guilds` tables
 CREATE TABLE meetup_groups_guilds (
-	id NUMERIC(32) NOT NULL UNIQUE,
-	PRIMARY KEY (id),
 	guild_id NUMERIC(20) NOT NULL,
-	meetup_group_id NUMERIC(12) NOT NULL
+	meetup_group_id NUMERIC(12) NOT NULL,
+	PRIMARY KEY (guild_id, meetup_group_id),
+	FOREIGN KEY (guild_id) REFERENCES guilds(guild_id) ON DELETE CASCADE,
+	FOREIGN KEY (meetup_group_id)  REFERENCES meetup_groups(group_id) ON DELETE CASCADE
 );
 
 -- Create 'linker' table between `discord_events` and `meetup_events` tables
 CREATE TABLE discord_events_meetup_events (
-	id NUMERIC(32) NOT NULL UNIQUE,
-	PRIMARY KEY (id),
 	discord_event_id NUMERIC(20) NOT NULL,
-	meetup_event_id NUMERIC(12) NOT NULL
+	meetup_event_id NUMERIC(12) NOT NULL,
+	PRIMARY KEY (discord_event_id, meetup_event_id),
+	FOREIGN KEY (discord_event_id) REFERENCES discord_events(discord_event_id) ON DELETE CASCADE,
+	FOREIGN KEY (meetup_event_id) REFERENCES meetup_events(meetup_event_id) ON DELETE CASCADE
 );
