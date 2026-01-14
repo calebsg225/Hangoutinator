f18914eb05c6568d7336b3061eca69370a76a012
calebsg225 <calebsg2253284@gmail.com>
2026-01-13 22:35:45 -0500

    flush out `meetup track` and `meetup untrack` discord commands

--- src/commands/meetup/track.rs
+++ src/commands/meetup/track.rs
@@ -1,21 +1,64 @@
 //! src/commands/meetup/track.rs
 
-//use sqlx::types::BigDecimal;
+use sqlx::types::BigDecimal;
 
-//use crate::commands::_util as util;
+use crate::commands::_util as util;
 use crate::{Context, Error};
 
 #[poise::command(slash_command, rename = "track")]
 pub async fn command(
     ctx: Context<'_>,
-    #[description = "the name of the meetup group to track"] _group_name: String,
+    #[description = "the name of the meetup group to track"] group_name: String,
 ) -> Result<(), Error> {
-    let _pool = &ctx.data().pool;
-    let _guild_id = ctx.guild_id().unwrap().get();
-    // - check if the group exists in db. if not, add.
-    // - if it does, check if guild is already tracking
-    // - if not, track and update events accordingly
-    // ^^^ NO ^^^
-    // there should be a different command whose sole purpose is to force-resync events
+    let pool = &ctx.data().pool;
+    let guild_id = BigDecimal::from(ctx.guild_id().unwrap().get());
+    let tracked_group = sqlx::query!(
+        "SELECT * FROM meetup_groups_guilds WHERE guild_id = $1 AND group_name = $2",
+        guild_id,
+        group_name
+    )
+    .fetch_optional(pool)
+    .await?;
+
+    if let Some(_) = tracked_group {
+        util::send_reply(
+            &ctx,
+            true,
+            &format!("Meetup group `{group_name}` is already being tracked!"),
+        )
+        .await?;
+        return Ok(());
+    }
+
+    let meetup_group = sqlx::query!(
+        "SELECT * FROM meetup_groups WHERE group_name = $1",
+        group_name
+    )
+    .fetch_optional(pool)
+    .await?;
+
+    if let None = meetup_group {
+        sqlx::query!(
+            "INSERT INTO meetup_groups (group_name) VALUES ($1)",
+            group_name
+        )
+        .execute(pool)
+        .await?;
+    }
+
+    sqlx::query!(
+        "INSERT INTO meetup_groups_guilds (guild_id, group_name) VALUES ($1, $2)",
+        guild_id,
+        group_name
+    )
+    .execute(pool)
+    .await?;
+
+    util::send_reply(
+        &ctx,
+        true,
+        &format!("You are now tracking the meetup group `{group_name}`."),
+    )
+    .await?;
     Ok(())
 }
--- src/commands/meetup/untrack.rs
+++ src/commands/meetup/untrack.rs
@@ -1,8 +1,59 @@
 //! src/commands/meetup/untrack.rs
 
+use sqlx::types::BigDecimal;
+
+use crate::commands::_util as util;
 use crate::{Context, Error};
 
 #[poise::command(slash_command, rename = "untrack")]
-pub async fn command(ctx: Context<'_>) -> Result<(), Error> {
+pub async fn command(
+    ctx: Context<'_>,
+    #[description = "the name of the meetup group to stop tracking"] group_name: String,
+) -> Result<(), Error> {
+    let pool = &ctx.data().pool;
+    let guild_id = BigDecimal::from(ctx.guild_id().unwrap().get());
+
+    let tracked_group = sqlx::query!(
+        "SELECT * FROM meetup_groups_guilds WHERE guild_id = $1 AND group_name = $2",
+        guild_id,
+        group_name,
+    )
+    .fetch_optional(pool)
+    .await?;
+
+    if let None = tracked_group {
+        util::send_reply(
+            &ctx,
+            true,
+            &format!("You are not currently tracking meetup group `{group_name}`."),
+        )
+        .await?;
+        return Ok(());
+    }
+
+    let tracking_guilds = sqlx::query!(
+        "SELECT * FROM meetup_groups_guilds WHERE guild_id != $1 AND group_name = $2",
+        guild_id,
+        group_name
+    )
+    .fetch_optional(pool)
+    .await?;
+
+    if let None = tracking_guilds {
+        sqlx::query!(
+            "DELETE FROM meetup_groups WHERE group_name = $1",
+            group_name
+        )
+        .execute(pool)
+        .await?;
+    } else {
+        sqlx::query!(
+            "DELETE FROM meetup_groups_guilds WHERE guild_id = $1 AND group_name = $2",
+            guild_id,
+            group_name
+        )
+        .execute(pool)
+        .await?;
+    }
     Ok(())
 }
